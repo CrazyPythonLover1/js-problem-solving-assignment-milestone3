e131d16204cf3cbd4014fd1c66a1ff7d279b531f
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-07-17 08:24:02 +0600

    solved feetToMile

--- assignment.js
+++ assignment.js
@@ -2,3 +2,18 @@
 // woodCalculator
 // brickCalculator
 // tinyFriend
+
+
+function feetToMile(feet){
+    if (feet>0){
+        var mile = feet / 5280;
+        mile = mile.toFixed(9);
+        return mile;
+    }
+    else{
+        return " WARNING! Distance wouldn't be negative or zero."
+    }
+    
+}
+var checkMile = feetToMile(30000);
+console.log(checkMile);
